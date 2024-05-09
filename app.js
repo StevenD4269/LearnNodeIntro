b047f30e2bd93b05cfc803ae1a27ba18c3de7360
Steven Dickerson <poddocdan@gmail.com>
2024-05-09 09:01:04 -0700

    final submission

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// importing a built in module called Http
-//This is a core function of node
-
+// import http from "http"
+// here written as CommonJS
 const http = require("http");
-//We use that http object to create a server
-//request and response are the most important two objects in all of node
-//they hold all the relevant info for server communication
+
 http.createServer((request, response) => {
-    //This line is sending back a response to whoever visited the server
-    response.end("Hey this is the server talking more");
-})
-//The .listen tells the server to startr and listen for incoming requests
-.listen(5000, () => { console.log("Server is now listening on port 5000"); })
+  response.writeHead(200, { "content-type": "text/html" });
+  response.write("Hello World!");
+  response.end();
+}).listen(5000, () => {
+  console.log("Server listening at http://localhost:5000...");
+});
